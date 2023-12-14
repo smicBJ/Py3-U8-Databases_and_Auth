1c68c63c0fabbe5d63db6c3ca43d4c1f27f27902
Jonathan Fernandes <jhfernan@mail.usf.edu>
2023-12-14 12:05:25 +0800

    Completed tasks route

--- routers/tasks.py
+++ routers/tasks.py
@@ -11,11 +11,11 @@ router = APIRouter()
 
 class Task(BaseModel):
     id: int | None = None
-    title: str
-    author: str
-    description: str
-    priority: int
-    complete: bool
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=2)
+    description: str = Field(min_length=3, max_length=250)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool = Field(default=False)
     created_on: datetime = datetime.utcnow()
 
     class Config:
@@ -30,72 +30,50 @@ class Task(BaseModel):
         }
 
 
-def create_id(task: Task):
-    task.id = 1 if len(TASKS) < 1 else TASKS[-1].id + 1
-    return task
-
-
-TASKS = [
-    Task(**{
-        "id": 1,
-        "title": "Unit 8",
-        "description": "In order to create more robust API's, we need to finish teaching Unit 8",
-        "author": "Jonathan Fernandes",
-        "priority": 5,
-        "complete": False
-    }),
-    Task(**{
-        "id": 2,
-        "title": "Buy Flowers",
-        "description": "Since Mrs. Fernandes has been so mean to her students, I will try to cheer her up",
-        "author": "Jonathan Fernandes",
-        "priority": 2,
-        "complete": False
-    })
-]
-
-
-@router.get("")
+@router.get("", status_code=status.HTTP_200_OK)
 async def get_all_tasks(db: Session = Depends(get_db)):
     return db.query(Tasks).all()
 
 
-@router.post("")
-async def create_task(task_data: Task):
-    new_task = Task(**task_data.model_dump())
-    TASKS.append(create_id(new_task))
-    return new_task
+@router.post("", status_code=status.HTTP_201_CREATED)
+async def create_task(task_data: Task, db: Session = Depends(get_db)):
+    new_task = Tasks(**task_data.model_dump())
+
+    db.add(new_task)
+    db.commit()
 
 
-@router.get("/{task_id}")
-async def get_task_by_id(task_id: int):
-    for task in TASKS:
-        if task_id == task.id:
-            return task
-    return {"msg": f"Task not found with id#{task_id}"}
+@router.get("/{task_id}", status_code=status.HTTP_200_OK)
+async def get_task_by_id(task_id: int = Path(gt=0), db: Session = Depends(get_db)):
+    task = db.query(Tasks).filter(Tasks.id == task_id).first()
+    if task is not None:
+        return task
+    raise HTTPException(status_code=404, detail=f"Task with id #{task_id} was not found")
 
 
-@router.put("/{task_id}")
-async def update_task_by_id(task_id: int, task_data: Task):
-    task_updated = False
-    for index in range(len(TASKS)):
-        if task_id == TASKS[index].id:
-            TASKS[index].title = task_data.title
-            TASKS[index].author = task_data.author
-            TASKS[index].description = task_data.description
-            TASKS[index].priority = task_data.priority
-            TASKS[index].complete = task_data.complete
-            task_updated = True
-    if not task_updated:
-        return {"msg": f"Task not found with id#{task_id}"}
+@router.put("/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_task_by_id(task_data: Task, task_id: int = Path(gt=0), db: Session = Depends(get_db)):
+    task = db.query(Tasks).filter(Tasks.id == task_id).first()
+
+    if task is None:
+        raise HTTPException(status_code=404, detail=f"Task with id #{task_id} was not found")
+
+    task.title = task_data.title
+    task.author = task_data.author
+    task.description = task_data.description
+    task.priority = task_data.priority
+    task.complete = task_data.complete
+
+    db.add(task)
+    db.commit()
 
 
-@router.delete("/{task_id}")
-async def delete_task_by_id(task_id: int):
-    task_deleted = False
-    for index in range(len(TASKS)):
-        if task_id == TASKS[index].id:
-            TASKS.pop(index)
-            task_deleted = True
-    if not task_deleted:
-        return {"msg": f"Task not found with id#{task_id}"}
+@router.delete("/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_task_by_id(task_id: int = Path(gt=0), db: Session = Depends(get_db)):
+    delete_task = db.query(Tasks).filter(Tasks.id == task_id).first()
+
+    if delete_task is None:
+        raise HTTPException(status_code=404, detail=f"Task with id #{task_id} was not found")
+
+    db.query(Tasks).filter(Tasks.id == task_id).delete()
+    db.commit()
